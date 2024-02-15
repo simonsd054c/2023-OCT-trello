00d497a21caf5b71de8dfa79de7db2ed11be7e0a
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-02-15 20:56:57 +1100

    ordered in card schema, and get all cards in card controller

--- models/card.py
+++ models/card.py
@@ -31,6 +31,7 @@ class CardSchema(ma.Schema):
 
     class Meta:
         fields = ('id', 'title', 'description', 'date', 'status', 'priority', 'user')
+        ordered = True
 
 card_schema = CardSchema()
 cards_schema = CardSchema(many=True)
