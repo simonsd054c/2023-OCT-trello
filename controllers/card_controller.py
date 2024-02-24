af1523272ff03a8497c1b56b4339668ff66933bc
Simon Dahal <simon.dahal@coderacademy.edu.au>
2024-02-24 12:25:21 +1100

    Authorisation

--- controllers/card_controller.py
+++ controllers/card_controller.py
@@ -5,6 +5,7 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 
 from init import db
 from models.card import Card, cards_schema, card_schema
+from models.user import User
 from controllers.comment_controller import comments_bp
 
 cards_bp = Blueprint('cards', __name__, url_prefix='/cards')
@@ -51,7 +52,12 @@ def create_card():
 
 # https://localhost:8080/cards/6 - DELETE
 @cards_bp.route('/<int:card_id>', methods=["DELETE"])
+@jwt_required()
 def delete_card(card_id):
+    # check user's admin status
+    is_admin = is_user_admin()
+    if not is_admin:
+        return {"error": "Not authorised to delete a card"}, 403
     # get the card from the db with id = card_id
     stmt = db.select(Card).where(Card.id == card_id)
     card = db.session.scalar(stmt)
@@ -69,6 +75,7 @@ def delete_card(card_id):
     
 # http://localhost:8080/cards/5 - PUT, PATCH
 @cards_bp.route('/<int:card_id>', methods=["PUT", "PATCH"])
+@jwt_required()
 def update_card(card_id):
     # Get the data to be updated from the body of the request
     body_data = card_schema.load(request.get_json(), partial=True)
@@ -77,6 +84,8 @@ def update_card(card_id):
     card = db.session.scalar(stmt)
     # if card exists
     if card:
+        if str(card.user_id) != get_jwt_identity():
+            return {"error": "Only the owner can edit the card"}, 403
         # update the fields
         card.title = body_data.get('title') or card.title
         card.description = body_data.get('description') or card.description
@@ -91,3 +100,9 @@ def update_card(card_id):
         # return error msg
         return {'error': f'Card with id {card_id} not found'}, 404
     
+
+def is_user_admin():
+    user_id = get_jwt_identity()
+    stmt = db.select(User).filter_by(id=user_id)
+    user = db.session.scalar(stmt)
+    return user.is_admin
